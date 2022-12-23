e64308be912006dbe263d578ef0a989cb38be090
saransh9174 <70794556+saransh9174@users.noreply.github.com>
2022-12-23 13:30:59 +0530

    Add files via upload

---
+++ q5.py
@@ -0,0 +1,11 @@
+l=[]
+
+n=int(input("Enter the size of array: "))
+print("accepted")
+for i in range(n):
+    ele=int((input("Enter element for array: ")))
+    l.append(ele)
+    print(l)
+for i in range(len(l)):
+    print("sum: ")
+    print(sum(l)/n)   
